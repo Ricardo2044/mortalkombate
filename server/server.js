b912da000f809060afc482b3a60deb17000e79e1
Ricardo2044 <ricardinhof200406@gmail.com>
2023-06-05 18:28:51 -0300

    criando projeto mortal kombat

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const socketIo = require("socket.io");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+// const GameCollection = require("./games.js");
+const port = 4545;
+
+app.configure(() => {
+  app.use(express.static(__dirname + "/../game"));
+});
+
+server.listen(port, () => {
+  console.log(`Rodando no link: http://localhost:${port}`);
+});
+
+const Responses = {
+  SUCCES: 0,
+  GAME_EXIST: 1,
+  GAME_NOT_EXIST: 2,
+  GAME_FULL: 3,
+};
+
+Requests = {
+  CREATE_GAME: "create-game",
+  JOIN_GAME: "join-game",
+};
+
+io.sockets.on("connection", (socket) => {
+  socket.on(Requests.CREATE_GAME, (gameName) => {
+    if (games.createGame(gameName)) {
+      games.getName(gameName).addPlayer(socket);
+      socket.emit("response", Responses.SUCCES);
+    } else {
+      socket.emit("response", Responses.GAME_EXIST);
+    }
+
+    socket.on(Requests.CREATE_GAME, (gameName) => {
+        
+    })
+  });
+});
